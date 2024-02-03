f7dda1bfcf72a1cf5206f88f220cd44468989e1a
Dinoplant <angel.delgado_jimenez23@student.cart.com>
2024-02-02 22:18:22 -0800

    Did the best I could

--- list.js
+++ list.js
@@ -1,17 +1,32 @@
-function arrayToList(arr) {
-  // Add code.
-}
-
-function listToArray(list, arr) {
-  // Add code.
-}
-
-// tests
-console.log(arrayToList([10, 20]));
-// → {value: 10, rest: {value: 20, rest: null}}
-console.log(listToArray(arrayToList([10, 20, 30])));
-// → [10, 20, 30]
-console.log(prepend(10, prepend(20, null)));
-// → {value: 10, rest: {value: 20, rest: null}}
-console.log(nth(arrayToList([10, 20, 30]), 1));
-// → 20
+function arrayToList(arr){
+  let list = null;  
+   for(let i = arr.length -1; i >= 0; i -= 1){
+    list = {value:arr[i], rest: list}
+   }
+ return list
+ }
+ 
+ function listToArray(list, arr){
+    arr= []
+   for (let i = list; i; i = i.rest) {
+    let c = -1
+     arr[c] = arr.push(i.value);
+  c++
+   }
+   return arr;
+ }
+ 
+ function prepend(list, place){
+ return {value: list, rest: place}
+ }
+ 
+ 
+ 
+ console.log(arrayToList([10, 20]));
+ // → {value: 10, rest: {value: 20, rest: null}}
+ console.log(listToArray(arrayToList([10, 20, 30])));
+ // → [10, 20, 30]
+ console.log(prepend(10, prepend(20, null)));
+ // → {value: 10, rest: {value: 20, rest: null}}
+ console.log(nth(arrayToList([10, 20, 30]), 1));
+ // → 20
