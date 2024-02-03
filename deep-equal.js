e3422ddf05e797a8b905ac958c59c63b837a048b
Dinoplant <angel.delgado_jimenez23@student.cart.com>
2024-02-02 22:19:04 -0800

    I tried the deep equal

--- deep-equal.js
+++ deep-equal.js
@@ -1,17 +1,17 @@
-function deepEqual(value, reference) {
-  // Add code.
-}
+function deepEqual(value, reference){
 
-
-
-
-
-
-// tests
-let obj = {here: {is: "an"}, object: 2};
-console.log(deepEqual(obj, obj));
-// → true
-console.log(deepEqual(obj, {here: 1, object: 2}));
-// → false
-console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
-// → true
+  if(value === reference){
+  return true
+  }
+    for(let i = 0; i <value.length; i++){
+  }
+    return Object.keys(value)
+  }
+  
+  let obj = {here: {is: "an"}, object: 2};
+  console.log(deepEqual(obj, obj));
+  // → true
+  console.log(deepEqual(obj, {here: 1, object: 2}));
+  // → false
+  console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+  // → true
